a478191d275f2cf43accfb489e949db8f866eb13
apascual <alberto.pascual.garcia@gmail.com>
2018-06-28 14:10:39 +0200

    Fixed condition to compute Wac, Wbc and checked loop

--- NodeSimilarity.pl
+++ NodeSimilarity.pl
@@ -16,35 +16,44 @@
 # We modify the similarity for both the Jaccard measure (which do
 # not consider a weighted network) and Tanimoto coefficients (weighted).
 # If your network is not weighted, the weight will be fixed to 1
-# and Tanimoto coefficients will be computed accordingly. You should, indicate
+# and Tanimoto coefficients will be computed accordingly. You should indicate
 # this in the "parameters" section of the code. Finally, if your network
 # is not weighted but you consider two types of nodes, you should
 # provide as "Weight" two values (+1 and +1) and tell to the
 # script that the network is weighted.
 #
 # INPUT: A TAB-separated file describing an undirected network of the format:
-#        NodeA   NodeB    Weight 
-#        where "Weight" may have sign, what will distinguish
-#        between the different types of links. In future versions we
-#        may add a field "Type" to consider further possibilities. It accepts
-#        header lines starting with '#'.
-#        
+#        NodeA   NodeB    Weight  Type
+#        "Weight" is the strength of the edge (can be positive or negative, if
+#        negative the absolute value will be taken in the computation of the Tanimoto coefficients).
+#        "Type" is an integer determining the type of edge (e.g. mutualistic=0, competitive=1).  
+#        It accepts an indefinite number of header lines starting with '#'.
+#        If the network has no weights, then the file should be formatted simply as:
+#        NodeA   NodeB   Type
+#
+# OPTIONS: -h  Prints a help message
+#          -W  equal to 0 if the network is not weighted, to 1 otherwise.
 # OUTPUT: A file describing a similarity matrix of the format:
 #         NodeA   NodeB   TanimotoCoeff  JaccardCoeff
 #
-# USAGE: ./NodeSimilarity $path2network
-#        In addition, there are some parameters you should control, 
-#        see the first section of the code.
+# USAGE: ./NodeSimilarity -W types $path2network
+#
+#        In addition, if you want to change the order of the input columns you can control it 
+#        in the first section "parameters".
 ########################################
-#TO BE DONE: Generalizar el script para considerar distintas "clases" a la hora de hacer la similaridad en NodeSimilarity.pl. 
-#Habría que añadir un campo de lectura (una columna más) que se utilice si quiere el usuario y, si no quiere, que use los signos de la interacción en el clustering.
+#
 #########################################
 # Silwood Park (Imperial College London)
 # July 4th, 2016. Alberto Pascual-García 
-# alberto.pascual.garcia@gmail.com 
+# alberto.pascual.garcia@gmail.com
+#
+# Updated in June, 2018.
+# Zürich, Theoretical Biology (ETH)
 #########################################
 #
+
 use POSIX;
+use Scalar::Util qw(looks_like_number);
 
 print "  \n";
 print "**************************************************  \n";
@@ -52,37 +61,37 @@ print "* Building nodes similarities NodeSimilarity.pl  *  \n";
 print "**************************************************  \n";
 print "  \n";
 #
-# --- Fix parameters here
+# ---  parameters here
 
-$Weighted=0; # One if there is a weighthed network, zero otherwise.
-$fieldNodeA=0; # Indicate the column where the first source node is found (minus one)
-$fieldNodeB=1; # where the target node is found (minus one)
-$fieldWeight=2; # and their interaction value (again minus one). If "Weighted" is zero it will check still if there
- # is a sign, and it will assign a +1 or -1 value
+
+&readParameters(@ARGV);
+
 
 # --- Print the information collected
 
-&printParameters($fieldEdgeA,$fieldEdgeB,$Weighted,$ARGV[0]);
+&printParameters($fieldNodeA,$fieldNodeB,$fieldType,$Weighted,$fileIn);
 
 # --- Read the network and build hashes
-
-foreach $line(@INTMP){ #  For each line "nodeA nodeB weight"
+$ctrl=-1;
+foreach $line(@INTMP){ #  For each line "nodeA nodeB weight type"
     #print join(' ',' ... Reading: ',$line),"\n";  # DEBUG
     if((substr($line,0,1) eq '#')||(substr($line,1,3)eq 'row')){ # skip header and col names
 	print join(' ','..Skip header: ',chomp($line)),"\n";
 	next;
     }
+    $ctrl+=1;
     chomp($line);
     @fields=split("\t",$line);
     $nodeA=$fields[$fieldNodeA];
     $nodeB=$fields[$fieldNodeB];
-    $weight=$fields[$fieldWeight];
+    $type=$fields[$fieldType];
+    if($ctrl==0){
+	if(!$type || !$fields[$fieldWeight]){print "~ Problems with file format, check type and weight fields. Exit...\n";exit;}		     
+    }
     if($Weighted==0){	
-	if($weight >0){
-	    $weight=1;
-	}else{
-	    $weight=-1;
-	}	  
+	$weight=1;	  
+    }else{
+	$weight=$fields[$fieldWeight];	    
     }
     $nodes2key{$nodeA}=1; # Store the nodes
     $nodes2key{$nodeB}=1;    
@@ -91,6 +100,8 @@ foreach $line(@INTMP){ #  For each line "nodeA nodeB weight"
     push(@{$edge2node{$edgeTmp}},$nodeB); # them from the new edge identifier, but this will be faster
     $edge2weight{$nodeA}{$nodeB}=$weight; # This structure basically codify every line
     $edge2weight{$nodeB}{$nodeA}=$weight;
+    $edge2type{$nodeA}{$nodeB}=$type; # This structure basically codify every line
+    $edge2type{$nodeB}{$nodeA}=$type;
     push(@{$neighbour{$nodeA}},$nodeB); # Store for every node its neighbours
     push(@{$neighbour{$nodeB}},$nodeA);    
 } # End foreach reading file
@@ -115,7 +126,7 @@ print "  \n";
 foreach $nodeTmp(@Nodes){ # For each node
     $AvWij=0;
     $Av2Wij=0;
-    @neighs=@{$neighbour{$nodeTmp}}; # This is an excess of cleaningness...
+    @neighs=@{$neighbour{$nodeTmp}}; # Just for peace of mind
     foreach $neighTmp(@neighs){ # Work with its neighbours
 	$Wij=$edge2weight{$nodeTmp}{$neighTmp}; # Recover the weights
 	$AvWij+=abs($Wij); # Store to compute the average of weights, no sign here (A_ii)
@@ -169,35 +180,46 @@ for($i=0; $i<$Nnodes; $i++){ # Edge 1
 	#     $Nab+=1;
 	# }
 	#... Next, check if they share any third
-	@neighsA = @{$neighbour{$nodeA}}; # Again, too clean? I do not like Perl brackets :-)
+	@neighsA = @{$neighbour{$nodeA}}; # Again, peace of mind
 	@neighsB = @{$neighbour{$nodeB}};
 	$selfctrl=0;
 	foreach $neighTmpA(@neighsA){ # Note that now each node is a neighbour of itself
-	    	foreach $neighTmpB(@neighsB){
-		    if($neighTmpA ne $neighTmpB){
-			next;
-		    }else{ # we still need to control that the link between both nodes if it exist is		       
-			if(($neighTmpA eq $nodeA)||($neighTmpA eq $nodeB)){ # counted just once
-			    if($selfctrl==0){
-				$selfctrl=1;
-			    }else{
-				next;
-			    }
+	    foreach $neighTmpB(@neighsB){
+		if($neighTmpA ne $neighTmpB){ # If they do not share the neighbour skip
+		    next;
+		}else{ # if it is the same, control if it is a link between both nodes (not with a neighbour) and if it is		       
+		    if(($neighTmpA eq $nodeA)||($neighTmpA eq $nodeB)){ # count it just once
+			if($selfctrl==0){
+			    $selfctrl=1;
+			}else{
+			    next;
 			}
 		    }
-		    if(defined($edge2weight{$nodeA}{$neighTmpA})){ # With the previous condition this
-			 if(defined($edge2weight{$nodeB}{$neighTmpB})){ # is not needed. Just to ctrl everything is ok
-			     $Wac=$edge2weight{$nodeA}{$neighTmpA};
-			     $Wbc=$edge2weight{$nodeB}{$neighTmpB};
-			     $Wprod=$Wac*$Wbc;
-			     #print join(' ',' *** Control: i, j, Wac, Wbc',$i,$j,$Wac,$Wbc),"\n"; # DEBUG
-			     if($Wprod>0){ # Both weights should have the same sign
-				 $Wab+=$Wprod;
-				 $Nab+=1;
-			     }
-			 }
-		    }	    
-		} # End foreach neighbour $j
+		}
+		# In any other situation it is a neighbour, we just need to control that the type is the same
+		if($edge2weight{$nodeA}{$neighTmpA}){ # This is implicit in the loop above and
+		    if($edge2weight{$nodeB}{$neighTmpB}){ # is not needed. Just to ctrl everything is ok
+			$pass=0; # control variable to count the shared neighbours
+			if(($nodeA eq $neighTmpA)||($nodeB eq $neighTmpB)){ # If it is the link between them we want to count it
+			    $pass=1;
+			}elsif($edge2type{$nodeA}{$neighTmpA} == $edge2type{$nodeB}{$neighTmpB}){ # also f they belong to the same type
+			    $pass=1;
+			}
+			if($pass == 1){ # Only those that pass the above filters
+			    $Wac=$edge2weight{$nodeA}{$neighTmpA};
+			    $Wbc=$edge2weight{$nodeB}{$neighTmpB};
+			    $Wprod=abs($Wac)*abs($Wbc);
+			    $Wab+=$Wprod;
+			    $Nab+=1;				
+			}
+			#if(!$edge2type{$nodeB}{$neighTmpB}){
+			#    print join(', ',' *** Control: nodeB',$nodeB,'tmpB',$neighTmpB,'Wbc',$edge2type{$nodeB}{$neighTmpB}),"\n"; # DEBUG
+			#    #exit;
+			#}
+			#print join(', ',' *** Control: i',$i,'j',$j,'Wac',$Wac,'Wbc',$Wbc),"\n"; # DEBUG
+		    }
+		}	    
+	    } # End foreach neighbour $j
 	} # End foreach neighbour $i
 	$NneighsA=$#{$neighbour{$nodeA}};#+1; # As usual, check this +1. Now is not needed, because we have already one more neigh (the node itself)
 	$NneighsB=$#{$neighbour{$nodeB}};#+1;	
@@ -228,6 +250,80 @@ print '  ',"\n";
 ###################################################
 ##                                       FUNCTIONS
 ###################################################
+######################
+#      readParameters
+######################
+# Print the different input parameters and choices to the standard output
+
+sub readParameters{
+    if($ARGV[0] eq "-h"){
+	&helpme();
+    }elsif($ARGV[0] eq "-W"){
+	if(!looks_like_number($ARGV[1])){
+	    print " \n";
+	    print ">> Input argument error for  NodeSimilarity.pl: \n";
+	    print $ARGV[1]," is not a valid Weight flag \n";
+	    print ">> run  ./NodeSimilarity.pl -h for help \n";
+	    print " \n";
+	    exit;
+	}else{
+	    $Weighted=$ARGV[1];
+	}
+	if(!$ARGV[2]){
+	    print " \n";
+	    print ">> Input argument error for NodeSimilarity.pl: \n";
+	    print ">> input file name missing \n";
+	    exit;
+	}else{	
+	    $fileIn=$ARGV[2];
+	}
+    }else{
+	if(looks_like_number($ARGV[0])){
+	    print " \n";
+	    print ">> Input argument error for NodeSimilarity.pl: \n";
+	    print $ARGV[0]," is not a valid file name \n";
+	    print ">> run  ./NodeSimilarity.pl -h for help \n";
+	    print " \n";
+	    exit;
+	}else{
+	    $fileIn=$ARGV[0];
+	}
+	if($ARGV[1] ne "-W"){
+	    print " \n";
+	    print ">> Input argument error for  NodeSimilarity.pl: \n";
+	    print " -W flag expected instead of $ARGV[1] \n";
+	    print ">> run  ./NodeSimilarity.pl -h for help \n";
+	    print " \n";
+	    exit;
+	}else{
+	    if(!looks_like_number($ARGV[2])){
+		print " \n";
+		print ">> Input argument error for  NodeSimilarity.pl: \n";
+		print $ARGV[2]," is not a valid Weight flag \n";
+		print ">> run  ./NodeSimilarity.pl -h for help \n";
+		print " \n";
+		exit;
+	    }else{
+		$Weighted=$ARGV[2];
+	    }
+	}
+    }
+    if($Weighted == 0){
+	$fieldNodeA=0; # Indicate the column where the first source node is found (minus one)
+	$fieldNodeB=1; # where the target node is found (minus one)
+	$fieldType=2;
+    }else{
+	$fieldNodeA=0; # Indicate the column where the first source node is found (minus one)
+	$fieldNodeB=1; # where the target node is found (minus one)
+	$fieldWeight=2; # and their interaction value (again minus one). 
+	$fieldType=3;
+    }    
+    if($Weighted==1){
+	return $fileIn, $Weighted, $fieldNodeA, $fieldNodeB,$fieldWeight,$fieldType;
+    }else{
+	return $fileIn, $Weighted, $fieldNodeA, $fieldNodeB,$fieldType;  
+    }
+}
 
 ######################
 #      printParameters
@@ -235,7 +331,7 @@ print '  ',"\n";
 # Print the different input parameters and choices to the standard output
 
 sub printParameters{
-    my ($fieldEdgeA,$fieldEdgeB,$Weighted,$pathIn)=@_;
+    my ($fieldNodeA,$fieldNodeB,$fieldType,$Weighted,$fileIn)=@_;
     print '~~~ Reading Node A from column ',$fieldNodeA+1,"\n";
     print '~~~ Reading Node B from column ',$fieldNodeB+1,"\n";
     if($Weighted==0){
@@ -243,10 +339,11 @@ sub printParameters{
     }else{
 	print '~~~ Working with a weighted network -- Tanimoto coefficient',"\n";
 	print '~~~ Reading weights from column ',$fieldWeight+1,"\n";
+	print '~~~ Reading types from column ',$fieldType+1,"\n";
     }
     print '  ',"\n";
     
-    $pathIn=$ARGV[0];
+    $pathIn=$fileIn;
     print join(' ','~~~ Input path: ',$pathIn),"\n";
     open(INTMP,$pathIn)  or die "Can't find file $pathIn\n";
     @fields=split("/",$pathIn);
@@ -265,8 +362,32 @@ sub printParameters{
 # Return the time where the script is being executed
 
 sub theTime{
-    ($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime();
+    ($second, $minute, $hour, $dayOfMonth, $month, $yearOffset) = localtime();
     $year = 1900 + $yearOffset;
     $theTime = " (mm,dd,yy) $month, $dayOfMonth, $year, and time: $hour:$minute:$second,";
     return $theTime;
+}
+
+######################
+#     helpme
+######################
+# Return the time where the script is being executed
+
+sub helpme{
+    print "\n";
+    print " > Help for NodeSimilarity.pl\n";
+    print " > USAGE: ./NodeSimilarity -W weighted path2network \n";
+    print "       - weighted: equal to one if weighted network, zero otherwise \n";
+    print "       - path2network: Path to a tab separated file with the following fields for weighted networks \n";
+    print "          NodeA   NodeB    Weight  Type \n";
+    print "         and, for unweighted networks formatted as: \n";
+    print "           NodeA   NodeB   Type     \n";
+    print "         where: \n";    
+    print "           -- Weight: is the strength of the edge (can be positive or negative, if \n";
+    print "                  negative the absolute value will be taken in the computation of the Tanimoto coefficients).\n";
+    print "           -- Type: is an integer determining the type of edge (e.g. mutualistic=0, competitive=1). \n"; 
+    print "       - It accepts an indefinite number of header lines starting with '#'.\n";
+    print "\n";
+    print "\n";
+    exit;
 }
